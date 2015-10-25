affd1210702b9ebe03ab806c2ec7b901eb35c96e
Dillon Hafer <dh@dillonhafer.com>
2015-10-25 00:32:11 -0500

    Add app index files.

---
+++ index.android.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry
+} = React;
+
+var Garage = require('./app/components/garage');
+
+AppRegistry.registerComponent('Garage', () => Garage);
