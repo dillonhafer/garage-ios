8384e09d4cc331535070790ed15cee8258f15aa8
Dillon Hafer <dh@dillonhafer.com>
2016-05-20 06:40:27 -0500

    Remove need to change status bar

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,7 @@
 import {
   AppRegistry,
-  StatusBar
 } from 'react-native';
 
 import Garage from './app/components/garage';
 
-StatusBar.setBarStyle('light-content', true);
 AppRegistry.registerComponent('Garage', () => Garage);
